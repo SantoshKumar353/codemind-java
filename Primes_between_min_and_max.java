e9634de92daaaae62465d4cc89b8ec007c4674e2
SantoshKumar353 <96779640+SantoshKumar353@users.noreply.github.com>
2022-09-27 00:47:30 +0530

    import java.util.Scanner; class Prime_MinMax {     public static boolean isprime(int n)     {         if(n==1)             return false;         for(int i=2;i<=(int)Math.sqrt(n);i++)             if(n%i==0)                 return false;         return true;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int arr[]=new int[n];         for(int i=0;i<n;i++)             arr[i]=sc.nextInt();         int min=0,max=0;         for(int i=0;i<n;i++)         {             if(arr[i]>arr[max])                 max=i;             if(arr[i]<arr[min])                 min=i;         }         int c=0;         for(int i=Math.min(min,max);i<=Math.max(min,max);i++)         {             if(isprime(arr[i]))                 c++;         }         System.out.print(c);     } }

---
+++ Primes_between_min_and_max.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class Prime_MinMax
+{
+    public static boolean isprime(int n)
+    {
+        if(n==1)
+            return false;
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
+            if(n%i==0)
+                return false;
+        return true;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+        int min=0,max=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>arr[max])
+                max=i;
+            if(arr[i]<arr[min])
+                min=i;
+        }
+        int c=0;
+        for(int i=Math.min(min,max);i<=Math.max(min,max);i++)
+        {
+            if(isprime(arr[i]))
+                c++;
+        }
+        System.out.print(c);
+    }
+}
