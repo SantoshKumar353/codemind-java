5eb36f7bf5c32b225d0032f7eb1f93824d12bbdf
SantoshKumar353 <96779640+SantoshKumar353@users.noreply.github.com>
2022-10-01 21:07:12 +0530

    import java.util.*; class sol {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         int sum=0;         for(int i=0;i<s.length();i++)         {             char ch=s.charAt(i);             if(Character.isDigit(ch))             {                 sum+=(int)ch-48;             }         }         System.out.println(sum);     } }

---
+++ Numbers_in_String.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class sol
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int sum=0;
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(Character.isDigit(ch))
+            {
+                sum+=(int)ch-48;
+            }
+        }
+        System.out.println(sum);
+    }
+}
