16b78ab2776e2f1d1cafe7c2f720806a3281dadf
SantoshKumar353 <96779640+SantoshKumar353@users.noreply.github.com>
2022-10-03 10:52:49 +0530

    import java.util.Scanner; class sol {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String d=sc.nextLine();         System.out.print(d.toLowerCase());     } }

---
+++ To_Lower_Case.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class sol
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String d=sc.nextLine();
+        System.out.print(d.toLowerCase());
+    }
+}
