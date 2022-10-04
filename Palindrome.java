bc56103587474f0e9b9fb284c3295d2648641c09
SantoshKumar353 <96779640+SantoshKumar353@users.noreply.github.com>
2022-10-04 19:19:49 +0530

    import java.util.*; class sol {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int temp=n,res=0,r;         while(temp>0)         {             r=temp%10;             res=(res*10)+r;             temp=temp/10;         }         if(res==n)         System.out.println("True");         else         System.out.println("False");     } }

---
+++ Palindrome.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class sol
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int temp=n,res=0,r;
+        while(temp>0)
+        {
+            r=temp%10;
+            res=(res*10)+r;
+            temp=temp/10;
+        }
+        if(res==n)
+        System.out.println("True");
+        else
+        System.out.println("False");
+    }
+}
