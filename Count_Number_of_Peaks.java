7a973bf28668d123afe68d9a691c9c47c4da84db
SantoshKumar353 <96779640+SantoshKumar353@users.noreply.github.com>
2022-12-17 00:04:13 +0530

    import java.util.*; class Solution{ 	public static void main(String args[]) 	{ 		Scanner sc = new Scanner(System.in); 		int n = sc.nextInt(); 		double s= 0; 		if(n==3) 		{ 		    System.out.format("10"); 		} 		else if(n>3) 		{ 		    s = Math.pow(3,(n-3))*8*(n-2); 		    s+=s/4; 		    System.out.println((int)s); 		} 		else 		{ 		    System.out.println("0"); 		} 	} }

---
+++ Count_Number_of_Peaks.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		double s= 0;
+		if(n==3)
+		{
+		    System.out.format("10");
+		}
+		else if(n>3)
+		{
+		    s = Math.pow(3,(n-3))*8*(n-2);
+		    s+=s/4;
+		    System.out.println((int)s);
+		}
+		else
+		{
+		    System.out.println("0");
+		}
+	}
+}
